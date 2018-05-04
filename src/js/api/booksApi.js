f275da8590ce77b622f3bb2c1578b6a17a2ce685
ivan <ivanshyrokykh@gmail.com>
2018-05-04 12:18:17 +0300

    Implement remove book capabilities and refactor adding and editing.

--- src/js/api/booksApi.js
+++ src/js/api/booksApi.js
@@ -1,6 +1,8 @@
-const books = [];
+const storageItem = 'library';
+const books = JSON.parse(localStorage.getItem(storageItem)) || [];
 
 const generateId = (book) => (book.title + book.author).toLowerCase();
+const saveChanges = () => localStorage.setItem(storageItem, JSON.stringify(books));
 
 export default class BookApi {
   static addBook(book) {
@@ -13,20 +15,29 @@ export default class BookApi {
     }
 
     books.push(book);
+    saveChanges();
     return true;
   }
 
   static editBook(book, prevBookId) {
     book.id = generateId(book);
-    const bookIndex = books.findIndex(b => b.id == prevBookId.id);
 
+    // check if book after editing has the same id of already existing book
+    if (~books.findIndex(b => b.id == book.id)) {
+      return false;
+    }
+
+    const bookIndex = books.findIndex(b => b.id == prevBookId.id);
     books.splice(bookIndex, 1, book);
+    saveChanges();
+    return true;
   }
 
   static removeBook(bookId) {
     const bookIndex = books.findIndex(b => b.id == bookId);
 
     books.splice(bookIndex, 1);
+    saveChanges();
   }
 
   static getBooks() {
--- src/js/app.js
+++ src/js/app.js
@@ -4,7 +4,7 @@ import '../img/logo.png';
 import BookApi from './api/booksApi';
 
 const editBook = (e) => {
-  let bookId = $(e.target).parent('.book').attr('data-book-id');
+  let bookId = $(e.target).closest('.book').attr('data-book-id');
   let book = BookApi.getBooks().find(b => b.id == bookId);
 
   $('#modal-add-book-form').serializeArray().forEach(v => {
@@ -18,33 +18,43 @@ const editBook = (e) => {
 };
 
 const removeBook = (e) => {
-  let bookId = $(e.target).parent('.book').attr('data-book-id');
+  let book = $(e.target).closest('.book');
+  let bookId = book.attr('data-book-id');
   BookApi.removeBook(bookId);
+  book.parent().remove(); // book container wrap a book
 };
 
 const bookView = (book) => {
-  return [
-    $(`<img src="${book.cover}" class="img-fluid"/>`),
-    $(`<h4>${book.title}</h4>`),
-    $(`<p>${book.author}</p>`),
-    $(`<p>${book.year}</p>`),
-    $(`<button class="btn btn-outline-primary float-left">Edit book</button>`).on('click', editBook),
-    $(`<button class="btn btn-outline-warning float-right">Remove book</button>`).on('click', removeBook)
-  ];
+  let item = $(`<div class="card book" data-book-id="${book.id}"></div>`);
+  let itemContent = $(`<div class="card-body"></div>`);
+  itemContent.append(
+    $(`<h4 class="card-title">${book.title}</h4>`),
+    $(`<p class="card-text">${book.author}</p>`),
+    $(`<p class="card-text">${book.year}</p>`),
+    $(`<button class="btn btn-outline-primary btn-lg btn-block">Edit book</button>`).on('click', editBook),
+    $(`<button class="btn btn-outline-warning btn-lg btn-block">Remove book</button>`).on('click', removeBook)
+  );
+
+  return item.append(
+    $(`<img src="${book.cover}" class="card-img-top img-fluid" alt="${book.title + ' ' + book.author}"/>`),
+    itemContent
+  );
 };
 
 const renderBook = (book, edit = false, prevBookId = null) => {
-  let bookContainer = $(`<div class="col-sm-6 book col-md-3 text-center" data-book-id="${book.id}"></div>`);
+  let bookContainer = $(`<div class="col-sm-6 col-lg-3 text-center"></div>`);
 
   if (edit) {
     let alreadyAddedBook = $(`.book[data-book-id="${prevBookId}"]`);
-    bookContainer.append(...bookView(book));
-    alreadyAddedBook.replaceWith(bookContainer);
+    bookContainer.append(bookView(book));
+    alreadyAddedBook.parent().replaceWith(bookContainer);
   } else {
-    $(bookContainer).append(...bookView(book));
+    $(bookContainer).append(bookView(book));
     $('.book-catalog').append(bookContainer);
   }
 };
+
+const isFormValid = (form) => form[0].checkValidity();
 
 const addBook = () => {
   let book = {};
@@ -52,10 +62,14 @@ const addBook = () => {
   let modalAdd = $('#modal-add-book');
   let dataId = modalAdd.attr('data-book-id');
 
+  if (!isFormValid(form)) {
+    form.addClass('was-validated');
+    return;
+  }
+
   form.serializeArray().forEach((v) => book[v.name] = v.value);
 
-  if (dataId) {
-    BookApi.editBook(book, dataId);
+  if (dataId && BookApi.editBook(book, dataId)) {
     renderBook(book, true, dataId);
     modalAdd.modal('hide');
     modalAdd.removeAttr('data-book-id');
@@ -67,9 +81,10 @@ const addBook = () => {
 
 $(document).ready(() => {
   $('#modal-add-book-btn').on('click', addBook);
+
   $('#modal-add-book').on('hide.bs.modal', () => {
-    $('#modal-add-book-form').trigger('reset');
+    $('#modal-add-book-form').trigger('reset').removeClass('was-validated');
   });
+
+  BookApi.getBooks().forEach(b => renderBook(b));
 });
-
-
